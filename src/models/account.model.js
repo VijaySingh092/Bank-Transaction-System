864e4ec512a419591cbdb64d600e3ddd23e39178
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-07-18 13:21:35 +0530

    transaction routes, controller and models

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -7,10 +7,14 @@ const accountSchema = new mongoose.Schema({
         required:[true,"Account must be associated witha user"],
         index:true
     },
-    status:{enum:{
+    status:{
+    type:String,
+    enum:{
         values:["ACTIVE","FROZEN","CLOSED"],
-        message:"Status can be either ACTIVE , FROZEN or CLOSED"
-    }},
+        message:"Status can be either ACTIVE , FROZEN or CLOSED",
+    },
+      default:"ACTIVE"
+},
     currency:{
         type:String,
         required:[true,"Currency is required for creating an account"],
